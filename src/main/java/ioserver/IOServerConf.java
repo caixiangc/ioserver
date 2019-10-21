4f4c961bd58458f8e738f087d29e0bfb1508539d
蔡翔 <939387484@qq.com>
2019-10-21 20:34:22 +0800

    dev

--- src/main/java/ioserver/IOServerConf.java
+++ src/main/java/ioserver/IOServerConf.java
@@ -3,5 +3,5 @@ package ioserver;
 
 public class IOServerConf {
     public static final String ip = "192.168.0.228";
-    public static final String port = "12380";
+    public static final String port = "123123";
 }
